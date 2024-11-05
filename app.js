b21876abd6d369072db0b86d4316e01ba6022f54
Nishtha Priya <2000195@apps.nsd.org>
2024-11-05 04:33:51 +0000

    done

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ function plusSlides(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
+  
+  if (n > x.length) {slideIndex = 1};
   if (n < 1) {slideIndex = x.length} ;
+  
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+  
   x[slideIndex-1].style.display = "";
 }
