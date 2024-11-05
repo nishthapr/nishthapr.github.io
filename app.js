da9b1d4b5736e32eee37a6ba28e47ef18df93dd4
Nishtha Priya <2000195@apps.nsd.org>
2024-11-05 04:40:33 +0000

    done

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ function plusSlides(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides");
   
-  if (n > x.length) {slideIndex = 1};
-  if (n < 1) {slideIndex = x.length} ;
+  if (n > slides.length) {slideIndex = 1};
+  if (n < 1) {slideIndex = slides.length} ;
   
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  
-  x[slideIndex-1].style.display = "";
+
+  slides[slideIndex-1].style.display = "block";
 }
